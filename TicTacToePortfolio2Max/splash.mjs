73608a63e366a3201fd48004019a69dc57e33cf5
MaxBennitt <max-leander2@hotmail.com>
2024-10-03 00:49:30 +0200

    Updated graphics

--- TicTacToePortfolio2Max/splash.mjs
+++ TicTacToePortfolio2Max/splash.mjs
@@ -1,12 +1,14 @@
-const ART = `
- ______  ____   __      ______   ____    __      ______   ___     ___
-|      ||    | /  ]    |      | /    |  /  ]    |      | /   \\   /  _]
-|      | |  | /  /     |      ||  o  | /  /     |      ||     | /  [_
-|_|  |_| |  |/  /      |_|  |_||     |/  /      |_|  |_||  O  ||    _]
-  |  |   |  /   \\_       |  |  |  _  /   \\_       |  |  |     ||   [_
-  |  |   |  \\     |      |  |  |  |  \\     |      |  |  |     ||     |
-  |__|  |____\\____|      |__|  |__|__|\\____|      |__|   \\___/ |_____|
+import { ANSI } from "./ansi.mjs";
 
+const ART = `
+${ANSI.COLOR.RED} ______  ____   __     ${ANSI.COLOR.GREEN} ______   ____    __     ${ANSI.COLOR.BLUE} ______   ___     ___   ${ANSI.COLOR.YELLOW}  ___
+${ANSI.COLOR.RED}|      ||    | /  ]    ${ANSI.COLOR.GREEN}|      | /    |  /  ]    ${ANSI.COLOR.BLUE}|      | /   \\   /  _]  ${ANSI.COLOR.YELLOW} |   |
+${ANSI.COLOR.RED}|      | |  | /  /     ${ANSI.COLOR.GREEN}|      ||  o  | /  /     ${ANSI.COLOR.BLUE}|      ||     | /  [_   ${ANSI.COLOR.YELLOW} |   |
+${ANSI.COLOR.RED}|_|  |_| |  |/  /      ${ANSI.COLOR.GREEN}|_|  |_||     |/  /      ${ANSI.COLOR.BLUE}|_|  |_||  O  ||    _]  ${ANSI.COLOR.YELLOW} |   | 
+${ANSI.COLOR.RED}  |  |   |  /   \\_     ${ANSI.COLOR.GREEN}  |  |  |  _  /   \\_     ${ANSI.COLOR.BLUE}  |  |  |     ||   [_   ${ANSI.COLOR.YELLOW} |___|
+${ANSI.COLOR.RED}  |  |   |  \\     |    ${ANSI.COLOR.GREEN}  |  |  |  |  \\     |    ${ANSI.COLOR.BLUE}  |  |  |     ||     |  ${ANSI.COLOR.YELLOW}  ___ 
+${ANSI.COLOR.RED}  |__|  |____\\____|    ${ANSI.COLOR.GREEN}  |__|  |__|__|\\____|    ${ANSI.COLOR.BLUE}  |__|   \\___/ |_____|  ${ANSI.COLOR.YELLOW} |___|
+${ANSI.RESET}
 `
 
 function showSplashScreen() {
